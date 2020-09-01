f7b2ba6068deaf8d94eb4fd44c984835d30bf159
iadvertiseyou <iadvertiseyou@web.de>
2020-09-01 23:17:21 +0200

    Login Page created

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -16,7 +16,7 @@ class Auth extends React.Component {
     async initialLogin(email, password) {
         // call api
         try {
-            const response = await fetch('nutrition-nodejs.herokuapp.com:3000/api/v1/auth', {
+            const response = await fetch('nutrition-nodejs.herokuapp.com/api/v1/auth', {
                 method: 'POST',
                 mode: 'cors',
                 body: JSON.stringify({
