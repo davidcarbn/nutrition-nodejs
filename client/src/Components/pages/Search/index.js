5c077872bc9397a01649fcb0ff1926cace38e0c6
iadvertiseyou <iadvertiseyou@web.de>
2020-10-11 15:09:51 +0200

    search css changed

--- client/src/Components/pages/Search/index.js
+++ client/src/Components/pages/Search/index.js
@@ -4,11 +4,12 @@ import './Search.css'
 import { Link } from 'react-router-dom'
 import Header from '../../Header'
 import BackButton from '../../Header/BackButton'
+import Container from '../../Container'
 const SearchProt = () => {
     return (
         <div className="Search-container">
             <div className="search-heading">
-                <input className="food-search"/>
+                <input className="food-search" />
             </div>
         </div>
     )
@@ -18,7 +19,7 @@ const Search = (props) => {
     const [result2, setResult2] = useState([])
     const searchFood = async (event) => {
         try {
-            const food= event.target.value
+            const food = event.target.value
             const reqPub = Axios.request({
                 method: "POST",
                 url: '/api/v1/search/' + food,
@@ -28,12 +29,12 @@ const Search = (props) => {
                 method: "POST",
                 url: '/api/v1/search/' + food,
                 baseURL: process.env.REACT_APP_BASE_URL,
-                data: {searchOwn: true}
+                data: { searchOwn: true }
             })
-            const responses = await Axios.all([reqPub,reqOwn])
+            const responses = await Axios.all([reqPub, reqOwn])
             setResult(responses[0].data.foods)
             setResult2(responses[1].data.foods)
-            
+
         } catch (error) {
             console.log(error)
         }
@@ -51,46 +52,59 @@ const Search = (props) => {
     }
     return (
         <>
-        <Header>
-            <BackButton target="/dashboard" />
-        </Header>
-        
-        <div className="Search-container">
-            <input className="food-search" type="text" onChange={searchFood} />
-            <ul className="list-heading">{result2 ?
-                result2.map((listItem) => (
-                    <li>
-                        <button data-foodid={listItem._id} onClick={redirectToFood}>
-                            <div>
-                                <div>{listItem.name}</div>
-                                <div>100g</div>
-                            </div>
-                            <div>
-                                {listItem.kcal}
-                            </div>
-                        </button>
-                    </li>
-                )) : "Kein Ergebnis"
-            }
-            </ul>
-            ------------------------------
-            <ul className="list-heading">{result ?
-                result.map((listItem) => (
-                    <li>
-                        <button data-foodid={listItem._id} onClick={redirectToFood}>
-                            <div>
-                                <div>{listItem.name}</div>
-                                <div>100g</div>
-                            </div>
-                            <div>
-                                {listItem.kcal}
-                            </div>
-                        </button>
-                    </li>
-                )) : "Kein Ergebnis"
-            }
-            </ul>
-        </div>
+            <Header>
+                <BackButton target="/dashboard" />
+            </Header>
+            <Container>
+
+                <input className="food-search" type="text" placeholder="Suchen..." onChange={searchFood} />
+                
+
+
+                    <ul className="search-result">
+                        <div className="result-heading">
+                            Gespeicherte Nahrungsmittel
+                        </div>
+                        {result2.length ?
+                            result2.map((listItem) => (
+                                <li>
+                                    <button data-foodid={listItem._id} onClick={redirectToFood}>
+                                        <div>
+                                            <div>{listItem.name}</div>
+                                        </div>
+                                        <div>
+                                            {listItem.kcal} kcal
+                                        </div>
+                                    </button>
+                                </li>
+                            )) : <div className="no-result">Kein Ergebnis</div>
+                        }
+                    </ul>
+                
+
+            <ul className="search-result">
+            <div className="result-heading">
+                            Vorschläge
+            </div>
+                {result.length ?
+                    result.map((listItem) => (
+                        <li>
+                            <button data-foodid={listItem._id} onClick={redirectToFood}>
+                                <div>
+                                    <div>{listItem.name}</div>
+                                    
+                                </div>
+                                <div>
+                                    {listItem.kcal} kcal
+                                </div>
+                            </button>
+                        </li>
+                    )) : <div className="no-result">Kein Ergebnis</div>
+                }
+                </ul>
+
+            </Container>
+
         </>
 
     )
