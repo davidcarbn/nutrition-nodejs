2b75c589682dbb240c42755ca10cab29e3f5aa0a
iadvertiseyou <iadvertiseyou@web.de>
2020-10-28 16:08:48 +0100

    css from px to em

--- client/src/Components/pages/Food/EditFood.js
+++ client/src/Components/pages/Food/EditFood.js
@@ -9,6 +9,10 @@ import './EditFood.css'
 import Food from '../../../objects/Food'
 import LabelInput from '../../Input/LabelInput'
 import Container from '../../Containers/Container'
+import Content from '../../Content'
+import LayoutContainer from '../../Containers/LayoutContainer'
+import LayoutContainerChild from '../../Containers/LayoutContainer/LayoutContainerChild'
+import ContainerChild from '../../Containers/Container/ContainerChild'
 const EditFood = (props) => {
     const { currentDate } = useDate()
 
@@ -82,8 +86,12 @@ const EditFood = (props) => {
                 </div>
             </Button>
         </Header>
-        <>{
-            !food ? "Loading..." : (
+        <Content>
+            <LayoutContainer>
+                <LayoutContainerChild>{
+                    !food ? <Container flexRow alignCenter>Loading...
+
+                    </Container> : (
                 <>
                 <Container>
                     <LabelInput 
@@ -95,17 +103,24 @@ const EditFood = (props) => {
                     />
                 </Container>
                     
-                    
-                    <FoodDetails
+                    <Container>
+                        <ContainerChild>
+                            <FoodDetails
                         food={food}
                         setFood={setFood}
                         amount={amount}
                     />
                     <input className="btn-delete" type="submit" onClick={deleteEntry} value="Eintrag löschen" />
+                        </ContainerChild>
+                    </Container>
+                    
                 </>
 
             )
-        }</></>
+        }
+                </LayoutContainerChild>
+            </LayoutContainer>
+            </Content></>
     )
 }
 export default EditFood
