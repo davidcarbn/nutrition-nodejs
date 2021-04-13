b1a92c534fcd11caec357ba689063287bfb321d6
iadvertiseyou <iadvertiseyou@web.de>
2021-04-13 21:13:10 +0200

    Search and sort added to List

--- client/src/Components/pages/Settings/CustomFood/index.js
+++ client/src/Components/pages/Settings/CustomFood/index.js
@@ -7,10 +7,12 @@ import LayoutContainerChild from '../../../Containers/LayoutContainer/LayoutCont
 import Content from '../../../Content'
 import Header from '../../../Header'
 import Button from '../../../Header/Button'
+import LabelInput from '../../../Input/LabelInput'
 import ButtonList from '../../../List/ButtonList'
 
 const CustomFood = (props) => {
     const [foodList, setFoodList] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
     useEffect(() => {
         const fetchCustomFood = async () => {
             const res = await Axios.request({
@@ -39,6 +41,11 @@ const CustomFood = (props) => {
                         <path d="M149.076,42.925l-5.6,5.6,5.6,5.6" transform="translate(-142.061 -41.511)" fill="none" stroke="#343540" stroke-linecap="round" stroke-width="2" />
                     </svg>
                 </Button>
+                <LabelInput
+                    label={false}
+                    onChange={e => setSearchTerm(e.currentTarget.value)}
+                    value={searchTerm}
+                    />
                 <Button target="/settings/customFood/add">
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
                         <g transform="translate(-305.5 -50.5)">
@@ -53,7 +60,17 @@ const CustomFood = (props) => {
                     <LayoutContainerChild>
                         <Container>
                             <ContainerChild>
-                                <ButtonList items={foodList} handleClick={handleClick} />
+                                <ButtonList 
+                                    items={
+                                        foodList
+                                            .filter((food) => (food.name.includes(searchTerm)))
+                                            .sort((a,b) => {
+                                                if (a.name < b.name) return -1
+                                                if (a.name > b.name) return 1
+                                                return 0
+                                            })
+                                        }
+                                    handleClick={handleClick} />
                             </ContainerChild>
                         </Container>
                     </LayoutContainerChild>
