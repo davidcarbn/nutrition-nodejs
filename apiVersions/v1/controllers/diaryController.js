6a777b8d54a9607c210c9b4771121303715a7c67
davidcarbn <davidcarbn@web.de>
2021-04-14 19:32:25 +0200

    added validation to diary endpoints

--- apiVersions/v1/controllers/diaryController.js
+++ apiVersions/v1/controllers/diaryController.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import Diary from '../../../models/Diary'
-
+import {isDate,isMongoId} from 'validator'
 
 const diaryController = express.Router()
 const getDiaryEntry = async (req, res) => {
     try {
         const {id} = req.user
         const {date} = req.params
+
+        // validation
+        if (!date || !isDate(new Date(date))) {
+            res.status(400).json({error: "Missing or Wrong arguments"})
+            return
+        }
         const diaryEntry = await Diary.findOne({user:id,date:date})
             .populate({path:'breakfast',populate: {path:'food'}})
             .populate({path:'lunch',populate: {path:'food'}})
@@ -32,26 +38,29 @@ const insertDiaryEntry = async (req, res) => {
             }
         }
         */
-       
+        if (!date || !isDate(new Date(date))) {
+            res.status(400).json({error: "missing or wromg arguments"})
+            return
+        }
         const  { breakfast, dinner, lunch, snacks } = entry
         if (!breakfast && !dinner && !lunch && !snacks) {
-            res.status(400).json({error: "Missing Arguments"})
+            res.status(400).json({error: "missing or wromg arguments"})
             return
         }
         if (breakfast && (!breakfast.food || !breakfast.amount)) {
-            res.status(400).json({error: "Missing Arguments"})
+            res.status(400).json({error: "missing or wromg arguments"})
             return
         }
         if (dinner && (!dinner.food || !dinner.amount)) {
-            res.status(400).json({error: "Missing Arguments"})
+            res.status(400).json({error: "missing or wromg arguments"})
             return
         }
         if (lunch && (!lunch.food || !lunch.amount)) {
-            res.status(400).json({error: "Missing Arguments"})
+            res.status(400).json({error: "missing or wromg arguments"})
             return
         }
         if (snacks && (!snacks.food || !snacks.amount)) {
-            res.status(400).json({error: "Missing Arguments"})
+            res.status(400).json({error: "missing or wromg arguments"})
             return
         }
        
@@ -75,7 +84,16 @@ const deleteDiaryEntry = async (req, res) => {
     try {
         const userId = req.user.id
         const {date,mealtime,id} = req.params
-        
+        // validation
+        if (!date || !isDate(new Date(date)) || !mealtime || typeof mealtime !== "string" || !id || !isMongoId(id)) {
+            res.status(400).json({error: "missing or wrong arguments"})
+            return
+        }
+        if (!["breakfast","dinner","lunch","snacks"].includes(mealtime)) {
+            res.status(400).json({error: "missing or wrong arguments"})
+            return
+        }
+
         let query = {
             user:userId,
             date:date
@@ -97,8 +115,20 @@ const updateDiaryEntry = async (req,res) => {
         const userId = req.user.id
         const {date,mealtime,id} = req.params
         const {amount} = req.body
-        const mealid = mealtime+'._id'
-        const mealamount =mealtime+'.$.amount'
+        //validation
+        if (!date || !isDate(new Date(date)) || !mealtime || typeof mealtime !== "string" || !id || !isMongoId(id)) {
+            res.status(400).json({error: "missing or wrong arguments"})
+            return
+        }
+        if (!["breakfast","dinner","lunch","snacks"].includes(mealtime)) {
+            res.status(400).json({error: "missing or wrong arguments"})
+            return
+        }
+        if (!amount || typeof amount !== "number") {
+            res.status(400).json({error: "missing or wrong arguments"})
+            return
+        }
+
         let query = {
             user:userId,
             date:date,
@@ -121,3 +151,4 @@ diaryController.post('/:date', insertDiaryEntry)
 diaryController.put('/:date/:mealtime/:id',updateDiaryEntry)
 diaryController.delete('/:date/:mealtime/:id', deleteDiaryEntry)
 export default diaryController
+
